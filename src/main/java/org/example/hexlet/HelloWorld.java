aa3932455d8890b1984f2c41b38a259b6b7d64bf
Матвей <matvei@MacBook-Pro-Matvej.local>
2024-08-04 23:08:26 +0300

    Update

--- src/main/java/org/example/hexlet/HelloWorld.java
+++ src/main/java/org/example/hexlet/HelloWorld.java
@@ -9,7 +9,12 @@ public class HelloWorld {
             config.bundledPlugins.enableDevLogging();
         });
         // Описываем, что загрузится по адресу /
-        app.get("/", ctx -> ctx.result("Hello World"));
-        app.start(7070); // Стартуем веб-сервер
+
+        app.get("/users", ctx -> {
+                var name = ctx.queryParamAsClass("name", String.class).getOrDefault("World");
+                ctx.result("Hello, " + name + "!");
+        });
+        app.post("/users1", ctx -> ctx.result("POST /users1"));
+        app.start(7070);
     }
 }
